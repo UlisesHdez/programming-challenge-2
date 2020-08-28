e7fbb8ab6844809901e4e477901667204200b056
Ulises Hernandez <ulisesxtigma@hotmail.com>
2020-08-28 15:35:24 -0500

    Solution base structure

---
+++ ComplexNumber.Tests/ComplexNumber_test.cs
@@ -0,0 +1,11 @@
+using Xunit;
+using ComplexNumber;
+
+namespace ComplexNumber.UnitTests
+{
+    public class ComplexNumber_Tests
+    {
+        private readonly ComplexNumber _complex;
+
+    }
+}
---
+++ ComplexNumber/Complex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ComplexNumber
+{
+    public class ComplexNumber
+    {
+        public double real;
+        public double imaginary;
+        public ComplexNumber(double realPart, double imaginaryPart)
+        {
+            this.real = realPart;
+            this.imaginary = imaginaryPart;
+        }
+
+    }
+}
